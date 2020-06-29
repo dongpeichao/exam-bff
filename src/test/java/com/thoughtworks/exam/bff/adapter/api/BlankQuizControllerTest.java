9cb4d74a0a87465e9f1a5cf35acddeea2d5fac00
peichao.dong <peichao.dong@thoughtuworks.com>
2020-06-29 21:08:22 +0800

    update configuration of tests

--- src/test/java/com/thoughtworks/exam/bff/adapter/api/BlankQuizControllerTest.java
+++ src/test/java/com/thoughtworks/exam/bff/adapter/api/BlankQuizControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,8 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureStubRunner
-@AutoConfigureMockMvc
+@AutoConfigureStubRunner(repositoryRoot = "stubs://file:///Users/peichao.dong/Documents/projects/dpc/sbux_jr/exam-quiz-stub/src/test/resources/contracts",
+        ids = "com.thoughtworks:exam-quiz-service-stubs:8100", generateStubs = true, stubsMode = StubRunnerProperties.StubsMode.REMOTE)@AutoConfigureMockMvc
 class BlankQuizControllerTest {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/thoughtworks/exam/bff/adapter/api/PaperControllerTest.java
+++ src/test/java/com/thoughtworks/exam/bff/adapter/api/PaperControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,7 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureStubRunner
+@AutoConfigureStubRunner(repositoryRoot = "stubs://file:///Users/peichao.dong/Documents/projects/dpc/sbux_jr/exam-quiz-stub/src/test/resources/contracts",
+        ids = "com.thoughtworks:exam-quiz-service-stubs:8100", generateStubs = true, stubsMode = StubRunnerProperties.StubsMode.REMOTE)
 @AutoConfigureMockMvc
 class PaperControllerTest {
     @Autowired
